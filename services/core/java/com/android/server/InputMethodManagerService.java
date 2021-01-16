02cc8f335507fe5b1064794cc3bfb15b8ad1de88
crdmod <crdmod@crdmod.mod>
2021-01-16 00:23:42 +0000

    base: Increase Longshot timeout to 2.5 seconds

--- services/core/java/com/android/server/InputMethodManagerService.java
+++ services/core/java/com/android/server/InputMethodManagerService.java
@@ -299,7 +299,7 @@ public class InputMethodManagerService extends IInputMethodManager.Stub
     }
 
     // Long screenshot
-    private long LONGSHOT_BLOCK_SHOWING_TIMEOUT = 1000;
+    private long LONGSHOT_BLOCK_SHOWING_TIMEOUT = 2500;
     public boolean mLongshotBlockShowing = false;
 
     final Context mContext;
